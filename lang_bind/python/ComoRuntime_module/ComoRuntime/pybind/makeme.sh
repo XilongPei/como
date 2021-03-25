523fc087f75c520ef1e3a24856e9e151e34c5463
XilongPei <pei_xilong@tongji.edu.cn>
2021-03-25 10:40:01 +0800

    python操作COMO的程序移至仓库https://gitee.com/tjopenlab/como_pybind

--- lang_bind/python/ComoRuntime_module/ComoRuntime/pybind/makeme.sh
+++
@@ -1,2 +0,0 @@
-#该编译命令会生成 como_pybind.cpython-35m-x86_64-linux-gnu.so
-g++ -O3 -Wall -shared -std=c++11 -fPIC `python3 -m pybind11 --includes` como_pybind.cpp -o gemfield`python3-config --extension-suffix`
