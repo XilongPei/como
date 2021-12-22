64ad59b9fd323686f67f4311be36260f0f4268f6
XilongPei <pei_xilong@tongji.edu.cn>
2021-12-22 19:13:31 +0800

    add ComoConfig::DBUS_BUS_CHECK_EXPIRES_PERIOD

--- como/runtime/ComoConfig.cpp
+++ como/runtime/ComoConfig.cpp
@@ -21,7 +21,11 @@ namespace como {
 int ComoConfig::ThreadPool_MAX_THREAD_NUM = 2;
 
 // ThreadPool_MAX_DBUS_DISPATCHER should less than ThreadPool_MAX_THREAD_NUM
+// perhaps it should be 1 always
 int ComoConfig::ThreadPool_MAX_DBUS_DISPATCHER = 1;
+
+// The period of detecting whether the object is overdue, in ns
+int ComoConfig::DBUS_BUS_CHECK_EXPIRES_PERIOD = 1000000000L * 60 * 2;
 
 int ComoConfig::ThreadPoolChannelInvoke_MAX_THREAD_NUM = 2;
 CpuInvokeDsa ComoConfig::cpuInvokeDsa[MAX_DSA_IN_ONE_SYSTEM] = {nullptr};
--- como/runtime/ComoConfig.h
+++ como/runtime/ComoConfig.h
@@ -37,6 +37,7 @@ class ComoConfig
 public:
     static int ThreadPool_MAX_THREAD_NUM;
     static int ThreadPool_MAX_DBUS_DISPATCHER;
+    static int DBUS_BUS_CHECK_EXPIRES_PERIOD;
     static int ThreadPoolChannelInvoke_MAX_THREAD_NUM;
     static int Logger_sLevel;
 
