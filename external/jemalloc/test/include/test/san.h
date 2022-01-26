d2bc2098a1110e072429f640f71f0b80f5d91296
XilongPei <pei_xilong@tongji.edu.cn>
2022-01-26 10:01:47 +0800

    remove jemalloc

--- external/jemalloc/test/include/test/san.h
+++
@@ -1,6 +0,0 @@
-static inline bool
-extent_is_guarded(tsdn_t *tsdn, void *ptr) {
-	edata_t *edata = emap_edata_lookup(tsdn, &arena_emap_global, ptr);
-	return edata_guarded_get(edata);
-}
-
--- external/jemalloc/test/unit/tcache_max.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-export MALLOC_CONF="tcache_max:1024,lg_san_uaf_align:-1"
