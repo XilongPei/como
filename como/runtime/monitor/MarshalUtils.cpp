73a66c615edc35dfc4f9e4c5edbacdd23807d0b3
XilongPei <pei_xilong@tongji.edu.cn>
2022-05-09 20:21:14 +0800

    MarshalUtils::UnMarshalArguments、MarshalUtils::UnUnmarshalResults生成数组时，数据更易读

--- como/runtime/monitor/MarshalUtils.cpp
+++ como/runtime/monitor/MarshalUtils.cpp
@@ -14,64 +14,92 @@
 // limitations under the License.
 //=========================================================================
 
+#include <stdlib.h>
 #include "util/comolog.h"
 #include "reflection/reflHelpers.h"
 #include "MarshalUtils.h"
 
 namespace como {
 
+static int TypeKindSizeOf(TypeKind typeKind)
+{
+    switch (typeKind) {
+        case TypeKind::Char:
+            return sizeof(Char);
+        case TypeKind::Byte:
+            return sizeof(Byte);
+        case TypeKind::Short:
+            return sizeof(Short);
+        case TypeKind::Integer:
+            return sizeof(Integer);
+        case TypeKind::Long:
+            return sizeof(Long);
+        case TypeKind::Float:
+            return sizeof(Float);
+        case TypeKind::Double:
+            return sizeof(Double);
+        case TypeKind::Boolean:
+            return sizeof(Boolean);
+        case TypeKind::HANDLE:
+            return sizeof(HANDLE);
+        case TypeKind::ECode:
+            return sizeof(ECode);
+        case TypeKind::Enum:
+            return sizeof(Integer);
+    }
+    return 0;
+}
+
 /* TripleHexDump() output:
 
 "array_var":["
-  0000  20 54 68 69 73 20 63 6c 61 73 73 20 69 73 20 75   This class is u
-  0010  73 65 64 20 74 6f 20 63 72 65 61 74 65 20 61 20  sed to create a
-  0020  68 65 78 20 64 75 6d 70 20 6f 66 20 61 6e 79 20  hex dump of any
-  0030  63 6f 6e 74 69 67 75 6f 75 73 20 6d 65 6d 6f 72  contiguous memor
-  0040  79 20 61 72 65 61 2e 20 54 68 65 20 68 65 78 20  y area. The hex
+  20 54 68 69 73 20 63 6c 61 73 73 20 69 73 20 75
+  73 65 64 20 74 6f 20 63 72 65 61 74 65 20 61 20
+  68 65 78 20 64 75 6d 70 20 6f 66 20 61 6e 79 20
 "]
 */
-static String& TripleHexDump(Triple& triple, String& strBuffer)
+static String& TripleHexDump(Triple& triple, String& strBuffer, int elemSize)
 {
-    const int perLine = 16;
+    const int perLine = abs(16 / elemSize);
 
     strBuffer.Reserve(triple.mSize * 4);
     strBuffer = "[\"\n";
 
-    int i;
-    unsigned char buff[perLine + 1];
     const unsigned char *pc = (const unsigned char *)triple.mData;
-
-    // Process every byte in the data.
-    for (i = 0;  i < triple.mSize;  i++) {
-        // Multiple of perLine means new or first line (with line offset).
+    for (int i = 0;  i < triple.mSize;) {
         if ((i % perLine) == 0) {
-            // Only print previous-line ASCII buffer for lines beyond first.
             if (i != 0)
-                strBuffer += String::Format("  %s\n", buff);
-
-            // Output the offset of current line.
-            strBuffer += String::Format("  %04x ", i);
+                strBuffer += String("\n");
         }
 
-        // Now the hex code for the specific character.
-        strBuffer += String::Format(" %02x", pc[i]);
-
-        // And buffer a printable ASCII character for later.
-        if ((pc[i] < 0x20) || (pc[i] > 0x7e)) // isprint() may be better.
-            buff[i % perLine] = '.';
-        else
-            buff[i % perLine] = pc[i];
-        buff[(i % perLine) + 1] = '\0';
+        switch (elemSize) {
+            case 2:
+                strBuffer += String::Format(" %04X", *(Short*)&pc[i]);
+                i += sizeof(Short);
+                break;
+            case 4:
+                strBuffer += String::Format(" %08lX", *(Integer*)&pc[i]);
+                i += sizeof(Integer);
+                break;
+            case 8:
+                strBuffer += String::Format(" %016llX", *(Long*)&pc[i]);
+                i += sizeof(Long);
+                break;
+            case -4:
+                strBuffer += String::Format(" %10.2f", *(Float*)&pc[i]);
+                i += sizeof(Float);
+                break;
+            case -8:
+                strBuffer += String::Format(" %16.2lf", *(Double*)&pc[i]);
+                i += sizeof(Double);
+                break;
+            default:
+                strBuffer += String::Format(" %02X", pc[i]);
+                i += sizeof(Byte);
+        }
     }
 
-    // Pad out last line if not exactly perLine characters.
-    while ((i % perLine) != 0) {
-        strBuffer += String::Format("   ");
-        i++;
-    }
-
-    // And print the final ASCII buffer.
-    strBuffer += String::Format("  %s\n\"]\n", buff);
+    strBuffer += String("\n\"]\n");
 
     return strBuffer;
 }
@@ -207,7 +235,8 @@ ECode MarshalUtils::UnMarshalArguments(
                     Triple value;
                     argParcel->ReadArray(&value);
                     String strTemp;
-                    strBuffer += MakeParamString(TripleHexDump(value, strTemp));
+                    strBuffer += MakeParamString(TripleHexDump(value, strTemp,
+                                                     TypeKindSizeOf(elemKind)));
                     break;
                 }
                 case TypeKind::Interface: {
@@ -358,7 +387,8 @@ ECode MarshalUtils::UnUnmarshalResults(
                     Triple value;
                     resParcel->ReadArray(&value);
                     String strTemp;
-                    strBuffer += MakeParamString(TripleHexDump(value, strTemp));
+                    strBuffer += MakeParamString(TripleHexDump(value, strTemp,
+                                                     TypeKindSizeOf(elemKind)));
                     break;
                 }
                 case TypeKind::Interface: {
@@ -390,7 +420,8 @@ ECode MarshalUtils::UnUnmarshalResults(
                     Triple value;
                     resParcel->ReadArray(&value);
                     String strTemp;
-                    strBuffer += MakeParamString(TripleHexDump(value, strTemp));
+                    strBuffer += MakeParamString(TripleHexDump(value, strTemp,
+                                                     TypeKindSizeOf(elemKind)));
                     break;
                 }
                 case TypeKind::Char:
