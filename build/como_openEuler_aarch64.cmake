a38166896f9401a61d45df8b41d684d5f0481edc
xilongpei <pei_xilong@tongji.edu.cn>
2024-08-30 14:57:43 +0800

    完善COMO on openEuler Embedded Linux 编译环境

--- build/como_openEuler_aarch64.cmake
+++ build/como_openEuler_aarch64.cmake
@@ -1,19 +1,24 @@
 
-set(CMAKE_SYSTEM_NAME Android)
+set(CMAKE_SYSTEM_NAME openEuler)
 set(CMAKE_SYSTEM_VERSION 1)
 
+SET(CMAKE_C_COMPILER_WORKS TRUE)
+SET(CMAKE_CXX_COMPILER_WORKS TRUE)
+
 set(GCC_DIR $ENV{ROOT}/prebuilt/aarch64-openeuler-linux/x86_64-openeulersdk-linux/usr)
-set(PREBUILT_DIR $ENV{ROOT}/prebuilt/android-80-pixel/sysroot)
+
+set(CROSS_PATH $ENV{ROOT}/prebuilt/aarch64-openeuler-linux/x86_64-openeulersdk-linux/usr/bin/aarch64-openeuler-linux-)
+set(CMAKE_SYSROOT $ENV{ROOT}/prebuilt/aarch64-openeuler-linux/prebuilt)
+
+set(PREBUILT_DIR $ENV{ROOT}/prebuilt/aarch64-openeuler-linux/prebuilt/aarch64-openeuler-linux)
 set(PREBUILT_INC ${PREBUILT_DIR}/usr/include)
 set(PREBUILT_LIB ${PREBUILT_DIR}/usr/lib64)
 
-set(CMAKE_C_COMPILER ${GCC_DIR}/bin/aarch64-openeuler-linux-gcc)
-set(CMAKE_CXX_COMPILER ${GCC_DIR}/bin/aarch64-openeuler-linux-g++)
+set(CMAKE_C_COMPILER ${CROSS_PATH}gcc)
+set(CMAKE_CXX_COMPILER ${CROSS_PATH}g++)
 
 set(CMAKE_SHARED_LIBRARY_PREFIX_CXX "" CACHE STRING "" FORCE)
 set(CMAKE_STATIC_LIBRARY_PREFIX_CXX "" CACHE STRING "" FORCE)
-
-set(CMAKE_SYSROOT ${PREBUILT_DIR})
 
 include_directories(
     ${PREBUILT_INC}
@@ -25,7 +30,9 @@ include_directories(
 set(COMMON_C_FLAGS "\
     -D__openEuler__ \
     -fPIC -ffunction-sections -fdata-sections -fstack-protector -fno-short-enums -fmessage-length=0 \
-    -no-canonical-prefixes -Wno-nullability-completeness -Wno-extern-c-compat")
+    -no-canonical-prefixes -Wno-nullability-completeness -Wno-extern-c-compat \
+    -mlittle-endian -fstack-protector-strong  -O2 -D_FORTIFY_SOURCE=2 -Wformat -Wformat-security \
+    -Werror=format-security --sysroot=${CMAKE_SYSROOT}")
 
 set(COMMON_CXX_FLAGS
     "${COMMON_C_FLAGS} -std=c++14 -fno-exceptions -fno-rtti")
@@ -33,17 +40,16 @@ set(COMMON_CXX_FLAGS
 set(DYNAMIC_LINKER
     "-Wl,-dynamic-linker,/system/bin/linker64")
 set(SO_CRT "\
-    ${PREBUILT_DIR}/usr/lib64/crtbegin_so.o \
+    ${PREBUILT_DIR}/prebuilt/usr/lib64/crtn.o \
     ${PREBUILT_DIR}/usr/lib64/crtend_so.o")
 set(EXE_CRT "\
-    ${PREBUILT_DIR}/usr/lib64/crtbegin_dynamic.o \
+    ${PREBUILT_DIR}/prebuilt/usr/lib64/crtn.o \
     ${PREBUILT_DIR}/usr/lib64/crtend_android.o")
 set(LIBC
     "-lc -lc++ -lcompiler_rt")
 
 set(COMMON_LINKER_FLAGS "\
     -B${GCC_DIR}/bin -fuse-ld=gold -nostdlib \
-    ${DYNAMIC_LINKER} \
     -Wl,-X -Wl,--gc-sections \
     ${LIBC}")
 
