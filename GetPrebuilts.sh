71b27a1fcc56062db27dd328f04e5ed7edeba282
xilongpei <pei_xilong@tongji.edu.cn>
2024-09-06 13:00:42 +0800

    add libunwind in GetPrebuilts.sh

--- GetPrebuilts.sh
+++ GetPrebuilts.sh
@@ -6,4 +6,7 @@ git clone https://gitee.com/tjopenlab/aarch64-openeuler-linux
 rm -rf icu4c
 git clone https://gitee.com/tjopenlab/icu4c
 
+rm -rf libunwind
+git clone https://gitee.com/tjopenlab/libunwind
+
 cd ..
