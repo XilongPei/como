bc366f1b5587a097bf35daf91bfdf1d3406d8d66
lsx <1215291835@qq.com>
2025-01-14 14:53:36 +0800

    implement the testHashmapCache program

--- test/runtime/util/testHashmapCache.cpp
+++ test/runtime/util/testHashmapCache.cpp
@@ -9,145 +9,136 @@ TEST(HashMapCache, TestPutAndGet)
 {
     como::HashMapCache<Integer, String> cache;
 
-    String value1 = "Value1";
-    String value2 = "Value2";
-    Integer i1 = 1;
-    Integer i2 = 2;
+    Integer key1 = 1, key2 = 2;
+    String value1 = "Value1", value2 = "Value2";
 
-    EXPECT_EQ(cache.Put(i1, value1, 10), 0);
-    EXPECT_EQ(cache.Put(i2, value2, 20), 0);
+    EXPECT_EQ(cache.Put(key1, value1, 10), 0);
+    EXPECT_EQ(cache.Put(key2, value2, 20), 0);
 
-    EXPECT_EQ(cache.Get(i1), value1);
-    EXPECT_EQ(cache.Get(i2), value2);
+    EXPECT_EQ(cache.Get(key1), value1);
+    EXPECT_EQ(cache.Get(key2), value2);
 }
 
-// TEST(HashMapCache, TestPutDuplicateKey)
-// {
-//     como::HashMapCache<int*, std::string*> cache;
+TEST(HashMapCache, TestPutDuplicateKey)
+{
+    como::HashMapCache<Integer, String> cache;
 
-//     int key = 1;
-//     std::string value1 = "Value1", value2 = "Value2";
-//     EXPECT_EQ(cache.Put(&key, &value1, 10), 0);
-//     EXPECT_EQ(cache.Put(&key, &value2, 10), 0);
-//     EXPECT_EQ(cache.Get(&key), &value2);
-// }
+    Integer key = 1;
+    String value1 = "Value1", value2 = "Value2";
+    EXPECT_EQ(cache.Put(key, value1, 10), 0);
+    EXPECT_EQ(cache.Put(key, value2, 10), 0);
+    EXPECT_EQ(cache.Get(key), value2);
+}
 
-// TEST(HashMapCache, TestRemove)
-// {
-//     como::HashMapCache<int*, std::string*> cache;
+TEST(HashMapCache, TestRemove)
+{
+    como::HashMapCache<Integer, String> cache;
 
-//     int key1 = 1, key2 = 2, key3 = 3;
-//     int *pk1 = &key1, *pk2 = &key2, *pk3 = &key3;
-//     std::string value1 = "Value1", value2 = "Value2";
-//     std::string *pv1 = &value1, *pv2 = &value2;
-//     cache.Put(pk1, pv1, 10);
-//     cache.Put(pk2, pv2, 10);
+    Integer key1 = 1, key2 = 2, key3 = 3;
+    String value1 = "Value1", value2 = "Value2", value3 = "Value3";
+    cache.Put(key1, value1, 10);
+    cache.Put(key2, value2, 10);
 
-//     EXPECT_EQ(cache.Remove(pk1), 1);
-//     EXPECT_EQ(cache.Remove(pk2), 1);
-//     EXPECT_EQ(cache.Remove(pk3), 0);
+    EXPECT_EQ(cache.Remove(key1), 1);
+    EXPECT_EQ(cache.Remove(key2), 1);
+    EXPECT_EQ(cache.Remove(key3), 0);
 
-//     EXPECT_EQ(cache.Get(pk1), std::string(0));
-//     EXPECT_EQ(cache.Get(pk2), std::string(0));
-// }
+    EXPECT_EQ(cache.Get(key1), String(0));
+    EXPECT_EQ(cache.Get(key2), String(0));
+}
 
-// TEST(HashMapCache, TestContainsKey)
-// {
-//     como::HashMapCache<int, std::string> cache;
+TEST(HashMapCache, TestContainsKey)
+{
+    como::HashMapCache<Integer, String> cache;
+    Integer key1 = 1, key2 = 2;
+    String value = "Value";
 
-//     cache.Put(1, "Value1", 10);
+    cache.Put(key1, value, 10);
 
-//     assert(cache.ContainsKey(1) == true);
-//     assert(cache.ContainsKey(2) == false);
-// }
+    EXPECT_EQ(cache.ContainsKey(key1), true);
+    EXPECT_EQ(cache.ContainsKey(key2), false);
+}
 
-// TEST(HashMapCache, TestClear)
-// {
-//     como::HashMapCache<int, std::string> cache;
+TEST(HashMapCache, TestClear)
+{
+    como::HashMapCache<Integer, String> cache;
 
-//     cache.Put(1, "Value1", 10);
-//     cache.Put(2, "Value2", 20);
+    Integer key1 = 1, key2 = 2;
+    String value1 = "Value1", value2 = "Value2";
+    cache.Put(key1, value1, 10);
+    cache.Put(key2, value2, 20);
 
-//     cache.Clear();
+    cache.Clear();
 
-//     assert(cache.Get(1) == "");
-//     assert(cache.Get(2) == "");
-// }
+    EXPECT_EQ(cache.ContainsKey(key1), false);
+    EXPECT_EQ(cache.ContainsKey(key2), false);
+}
 
-// TEST(HashMapCache, TestCleanUpExpiredData)
-// {
-//     como::HashMapCache<int, std::string> cache(10, 1);
+TEST(HashMapCache, TestCleanUpExpiredData)
+{
+    como::HashMapCache<Integer, String> cache(5, 2);
 
-//     cache.Put(1, "Value1", 10);
-//     sleep(2);
+    Integer key = 1;
+    String value = "Value1";
+    cache.Put(key, value, 10);
+    sleep(5);
 
-//     assert(cache.CleanUpExpiredData() == 1);
-//     assert(cache.Get(1) == "");
-// }
+    cache.CleanUpExpiredData();
+    // This test failure is expected because CHECK_EXPIRES_PERIOD is defined as 300 in HashMapCache. 
+    // If you modify CHECK_EXPIRES_PERIOD to a value smaller than the cleanup time, 
+    // it will allow the current test to pass.
+    EXPECT_EQ(cache.ContainsKey(key), false);
+}
 
-// void TestGetValues()
-// {
-//     como::HashMapCache<int, std::string> cache;
+TEST(HashMapCache, TestGetValues)
+{
+    como::HashMapCache<Integer, String> cache;
+    Integer key1 = 1, key2 = 2, key3 = 3;
+    String value1 = "Value1", value2 = "Value2", value3 = "Value3";
+    cache.Put(key1, value1, 10);
+    cache.Put(key2, value2, 20);
+    cache.Put(key3, value3, 20);
 
-//     // 插入元素
-//     cache.Put(1, "Value1", 10);
-//     cache.Put(2, "Value2", 20);
-//     cache.Put(3, "Value3", 30);
+    Array<String> ret = cache.GetValues();
+    EXPECT_EQ(ret.GetLength(), 3);
+}
 
-//     // 获取所有值
-//     auto values = cache.GetValues();
-//     assert(values.GetLength() == 3);  // 应该返回3个值
-//     assert(values.Get(0) == "Value1");
-//     assert(values.Get(1) == "Value2");
-//     assert(values.Get(2) == "Value3");
-// }
+TEST(HashMapCache, TestRemoveByValue)
+{
+    como::HashMapCache<Integer, String> cache;
 
-// void TestRemoveByValue()
-// {
-//     como::HashMapCache<int, std::string> cache;
+    Integer key1 = 1, key2 = 2;
+    String value1 = "Value1", value2 = "Value2";
+    cache.Put(key1, value1, 10);
+    cache.Put(key2, value2, 20);
 
-//     // 插入元素
-//     cache.Put(1, "Value1", 10);
-//     cache.Put(2, "Value2", 20);
+    EXPECT_EQ(cache.RemoveByValue(10), 1);
+    EXPECT_EQ(cache.RemoveByValue(30), 0);
 
-//     // 删除指定值的元素
-//     assert(cache.RemoveByValue(10) == 1);  // 删除值为10的元素
-//     assert(cache.RemoveByValue(30) == 0);  // 没有值为30的元素
+    // Confirm the element has been deleted.
+    EXPECT_EQ(cache.Get(1), String(0));
+    EXPECT_EQ(cache.Get(2), value2);
+}
 
-//     // 确认删除后的结果
-//     assert(cache.Get(1) == "");  // 键1的元素已删除
-//     assert(cache.Get(2) == "Value2");  // 键2的元素未删除
-// }
+TEST(HashMapCache, TestRehash)
+{
+    como::HashMapCache<Integer, String> cache(5);
 
-// void TestRehash()
-// {
-//     como::HashMapCache<int, std::string> cache(2);  // 初始容量为2
+    Integer key1 = 1, key2 = 2, key3 = 3, key4 = 4, key5 = 5;
+    String value1 = "Value1", value2 = "Value2", value3 = "Value3", value4 = "Value4", value5 = "Value5";
+    // trigger rehash function
+    cache.Put(key1, value1, 10);
+    cache.Put(key2, value2, 20);
+    cache.Put(key3, value3, 30);
+    cache.Put(key4, value4, 40);
+    cache.Put(key5, value5, 50);
 
-//     // 插入元素，触发重新哈希
-//     cache.Put(1, "Value1", 10);
-//     cache.Put(2, "Value2", 20);
-//     cache.Put(3, "Value3", 30);  // 会触发重新哈希
-
-//     // 确认所有元素仍然存在
-//     assert(cache.Get(1) == "Value1");
-//     assert(cache.Get(2) == "Value2");
-//     assert(cache.Get(3) == "Value3");
-// }
-
-// void TestGetValuesWithWalker()
-// {
-//     como::HashMapCache<int, std::string> cache;
-
-//     // 插入元素
-//     cache.Put(1, "Value1", 10);
-//     cache.Put(2, "Value2", 20);
-
-//     // 遍历所有键值对
-//     cache.GetValues([](std::string& str, int& key, std::string& value, struct timespec& time) {
-//         // 简单打印键值对
-//         std::cout << "Key: " << key << ", Value: " << value << std::endl;
-//     });
-// }
+    EXPECT_EQ(cache.Get(1), value1);
+    EXPECT_EQ(cache.Get(2), value2);
+    EXPECT_EQ(cache.Get(3), value3);
+    EXPECT_EQ(cache.Get(4), value4);
+    EXPECT_EQ(cache.Get(5), value5);
+}
 
 
 int main(int argc, char **argv)
