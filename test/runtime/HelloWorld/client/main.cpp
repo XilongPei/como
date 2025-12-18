9a56d465611c08fad9ade2684fb38ae7c5dbb759
xilongpei <pei_xilong@tongji.edu.cn>
2025-12-18 13:09:49 +0800

    add testcase HelloWorld

---
+++ test/runtime/HelloWorld/client/main.cpp
@@ -0,0 +1,44 @@
+//=========================================================================
+// Copyright (C) 2018 The C++ Component Model(COMO) Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//=========================================================================
+
+#include "como.demo.CFoo.h"
+#include "como.demo.IFoo.h"
+#include <comoapi.h>
+#include <comosp.h>
+#include <comoobj.h>
+#include <cstdio>
+#include <cstdlib>
+#include <gtest/gtest.h>
+
+using namespace como;
+
+using como::demo::CFoo;
+using como::demo::IFoo;
+
+TEST(TestIDfromName, testPrintf)
+{
+    InterfaceID iid = InterfaceIDWithMemArea(IID_IBar, 1);
+    AutoPtr<IFoo> foo;
+    ECode ec = CFoo::New(iid, (IInterface**)&foo);
+    EXPECT_EQ(ec, NOERROR);
+    foo->printf(String("Hello World\n"));
+}
+
+int main(int argc, char **argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
